c8e2ec25e059cb48751ff6d58723c6072d52fd4c
Gripka <gripkagripka@gmail.com>
2023-07-03 17:49:23 -0300

    pagina de cadastro

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -24,3 +24,49 @@ class LoginForms (forms.Form):
             }
         )
     )
+
+class CadastroForms(forms.Form):
+    nome_cadastro = forms.CharField(
+        label = "Nome de Cadastro",
+        required = True,
+        max_length = 100,
+        widget = forms.TextInput (
+            attrs = {
+                "class": "form-control",
+                "placeholder": "Ex: Luiz"
+            }
+        )
+    )
+    email = forms.EmailField(
+        label = "E-mail",
+        required = True,
+        max_length = 100,
+        widget = forms.EmailInput(
+            attrs = {
+                "class": "form-control",
+                "placeholder": "Ex: luiz@luiz.com"
+            }
+        )
+    )
+    senha_1 = forms.CharField(
+        label = "Senha",
+        required = True,
+        max_length = 70,
+        widget = forms.PasswordInput (
+            attrs = {
+                "class": "form-control",
+                "placeholder": "Digite sua senha"
+            }
+        )
+    )
+    senha_2 = forms.CharField(
+        label = "Confirme sua senha",
+        required = True,
+        max_length = 70,
+        widget = forms.PasswordInput (
+            attrs = {
+                "class": "form-control",
+                "placeholder": "Digite sua senha novamente"
+            }
+        )
+    )
